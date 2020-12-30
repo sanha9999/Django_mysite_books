6f1dcb006af25a21dec1dc86ff119ccb00a15244
SanHa Kang <sanha9999@naver.com>
2020-12-30 22:58:41 +0900

    테이블 생성

--- ch3/mysite/settings.py
+++ ch3/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books.apps.BooksConfig',        # 추가
 ]
 
 MIDDLEWARE = [
